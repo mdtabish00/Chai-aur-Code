1eafdd9350c4e425143172dbdc9679958b74df82
Sumit <moihd44tabish@gmail.com>
2024-05-26 06:56:39 +0530

    step -1

---
+++ test_project/src/App.jsx
@@ -0,0 +1,34 @@
+import { useCallback, useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setcharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*(){}~";
+
+    for (let i = 1; i <= array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounder-lg px-p4 my-8 text-orange-500 bg-gray-800">
+        Test
+      </div>
+    </>
+  );
+}
+
+export default App;
